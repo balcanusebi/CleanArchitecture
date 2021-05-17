c8852d632c76585bb5caacf4f57f2d4fcc095e4b
sebi_balcanu <sebi_balcanu@yahoo.com>
2021-05-17 14:57:22 +0300

    Presentation Layer

---
+++ CleanArchitecture/CleanArchitecture.MVC/Controllers/ToDoController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.MVC.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private IToDoService _toDoService;
+
+        public ToDoController(IToDoService toDoService)
+        {
+            _toDoService = toDoService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _toDoService.GetToDos();
+
+            return View(model);
+        }
+    }
+}
